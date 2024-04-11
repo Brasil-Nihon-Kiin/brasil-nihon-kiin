6403fe5a80e43815dee39f3397bdd078c4bbb363
Philippe Fanaro <philippefanaro@gmail.com>
2024-04-11 09:20:20 -0300

    countries and languages lists

--- src/lib/components/common/MultiSelect.tsx
+++ src/lib/components/common/MultiSelect.tsx
@@ -3,7 +3,9 @@ import { useState } from "react"
 import useCountries from "use-countries"
 
 export function MultiSelect() {
-  const { languages } = useCountries()
+  const { languages, countries } = useCountries()
+
+  console.log(countries)
 
   const [selectedLanguages, setSelectedLanguages] =
     useState<string[]>([])
@@ -11,11 +13,9 @@ export function MultiSelect() {
   function handleToggle(
     e: React.MouseEvent<HTMLInputElement>
   ) {
-    // @ts-ignore
-    const clickedLanguage = e.target.dataset.value
+    const clickedLanguage = (e.target as HTMLInputElement)
+      .dataset.value!
     const selectedLanguagesSet = new Set(selectedLanguages)
-
-    console.log(selectedLanguagesSet)
 
     if (selectedLanguagesSet.has(clickedLanguage)) {
       selectedLanguagesSet.delete(clickedLanguage)
@@ -28,36 +28,45 @@ export function MultiSelect() {
   }
 
   return (
-    <div className="dropdown w-full">
-      <div tabIndex={0} role="button" className="btn">
-        {selectedLanguages.length === 0
-          ? "Países de Origem"
-          : ""}
-        {selectedLanguages.map((l, i) => {
-          return (
-            <span
-              className="text-neutral-100 bg-accent p-1 rounded-xl"
-              key={i}
-            >
-              {l}
-            </span>
-          )
-        })}
+    <div className="w-full">
+      <div className="label">
+        <span className="label-text">Países de Origem</span>
       </div>
-      <div className="grid grid-cols-1 gap-2 max-h-[200px] w-52 mt-1 overflow-y-auto p-2 shadow menu dropdown-content z-[1] bg-base-200 rounded-box">
-        {languages.map((l, i) => {
-          return (
-            <div key={i} className="flex gap-2">
-              <input
-                type="checkbox"
-                className="checkbox"
-                data-value={l.name}
-                onClick={handleToggle}
-              />
-              <p>{l.name}</p>
-            </div>
-          )
-        })}
+      <div className="dropdown w-full">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn h-max p-2 w-full justify-start"
+        >
+          {selectedLanguages.length === 0
+            ? "Países de Origem"
+            : ""}
+          {selectedLanguages.map((l, i) => {
+            return (
+              <span
+                className="text-neutral-100 bg-accent p-[6px] rounded-xl"
+                key={i}
+              >
+                {l}
+              </span>
+            )
+          })}
+        </div>
+        <div className="grid grid-cols-1 gap-2 max-h-[200px] w-52 mt-1 overflow-y-auto p-2 shadow menu dropdown-content z-[1] bg-base-200 rounded-box">
+          {languages.map((l, i) => {
+            return (
+              <div key={i} className="flex gap-2">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  data-value={l.name}
+                  onClick={handleToggle}
+                />
+                <p>{l.name}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
--- src/lib/utils/exports.ts
+++ src/lib/utils/exports.ts
@@ -1,5 +1,7 @@
 export * from "./array"
+export * from "./countries"
 export * from "./elo"
+export * from "./languages"
 export * from "./prisma_utils"
 export * from "./server_utils"
 export * from "./string"
